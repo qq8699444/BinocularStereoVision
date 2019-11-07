543c2351a1e11b472137ad80f69e1c23b0a7b5fd
qq8699444 <699444@qq.com>
2019-11-07 23:16:41 +0800

    tasi stage 4

--- tsaiCalibration.cpp
+++ tsaiCalibration.cpp
@@ -108,6 +108,7 @@ int main(int argc, const char* argv[])
     float absty = sqrtf(ty2);
 
     //stage 3
+    float ty;
     {
         float r21 = a5 * absty;
         float r22 = a6 * absty;
@@ -125,13 +126,91 @@ int main(int argc, const char* argv[])
         float r13 = a3 * absty/sx;
         float tx = a4 * absty / sx;
 
-        const Point3f& srcpt = ptPairs[5].worldPt;
-        const Point2f& dstpt = ptPairs[5].imagePt;
+
+        // 选一个远离图像中心的点
+        const Point3f& srcpt = ptPairs[6].worldPt;
+        const Point2f& dstpt = ptPairs[6].imagePt;
 
         float x = r11 * srcpt.x + r12 * srcpt.y + r13 * srcpt.z + tx;
         float y = r21 * srcpt.x + r22 * srcpt.y + r23 * srcpt.z + absty;
 
         cout << "x:" << x << ", y: " << y  << endl;
+
+        if (dstpt.x * x > 0  && dstpt.y * y > 0)
+        {
+            ty = absty;
+        }
+        else if (dstpt.x * x < 0 && dstpt.y * y < 0)
+        {
+            ty = -absty;
+        }
+        else
+        {
+            cout << "wrong ...." << endl;
+            return -1;
+        }
+    }
+
+    //stage 4
+    float r21 = a5 * ty;
+    float r22 = a6 * ty;
+    float r23 = a7 * ty;
+
+    float a1 = result(0, 0);
+    float a2 = result(1, 0);
+    float a3 = result(2, 0);
+    float a4 = result(3, 0);
+
+    //sx must be positive
+    float sx = sqrtf(powf(a1*ty, 2) + powf(a2*ty, 2) + powf(a3*ty, 2));
+    float r11 = a1 * ty / sx;
+    float r12 = a2 * ty / sx;
+    float r13 = a3 * ty / sx;
+    float tx = a4 * ty / sx;
+
+
+    float r31 = r12 * r23 - r13 * r22;
+    float r32 = r13 * r21 - r11 * r23;
+    float r33 = r11 * r22 - r12 * r21;
+
+
+    float f, tz;
+    {
+       
+        Eigen::Matrix<double, ptCnt*2, 2>    matrixA2;
+        Eigen::Matrix<double, ptCnt*2, 1>    matrixb2;
+
+        for (int ptIdx = 0; ptIdx < ptCnt; ptIdx++)
+        {
+            const Point3f worldpt = ptPairs[ptIdx].worldPt;
+            const Point2f imagept = ptPairs[ptIdx].imagePt;
+
+            float xi = r11 * worldpt.x + r12 * worldpt.y + r13 * worldpt.z + tx;
+            float yi = r21 * worldpt.x + r22 * worldpt.y + r23 * worldpt.z + ty;
+            float wi = r31 * worldpt.x + r32 * worldpt.y + r33 * worldpt.z;
+
+            matrixA2(2 * ptIdx + 0, 0) = yi;
+            matrixA2(2 * ptIdx + 0, 1) = -imagept.y;
+            matrixb2(2 * ptIdx + 0, 0) = wi* imagept.y;
+
+            matrixA2(2 * ptIdx + 1, 0) = sx * xi;
+            matrixA2(2 * ptIdx + 1, 1) = -imagept.x;
+            matrixb2(2 * ptIdx + 1, 0) = wi * imagept.x;
+
+            
+        }
+
+        auto ATA2 = matrixA2.transpose() * matrixA2;
+        cout << "matrixA2:" << matrixA2 << endl;
+        cout << "ATA2 det:" << ATA2.determinant() << endl;
+
+        Eigen::Matrix<double, 2, 1> result2 = (matrixA2.transpose() * matrixA2).inverse() * matrixA2.transpose() * matrixb2;
+        cout << "result2:" << result2 << endl;
+
+        f = result2(0, 0);
+        tz = result2(1, 0);
+
+        cout << "f:" << f << ",tz:" << tz << endl;
     }
     cv::imshow("img", img);
     cv::waitKey(0);
